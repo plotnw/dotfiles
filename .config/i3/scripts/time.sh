9d57295870ff133e4655109d8e471b1ddbaa50fc
Weznon <waterwasp55555@gmail.com>
2016-11-22 15:07:09 -0500

    asd

--- .config/i3/scripts/time.sh
+++ .config/i3/scripts/time.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "<span color='#131e20' bgcolor='#4280c8' font_desc='FontAwesome'> </span><span font='9' color='#131e20' bgcolor='#4280c8'>$(date '+%m/%d/%y  %H:%M') </span>"
+echo "<span color='#131e20' bgcolor='#4280c8' font_desc='FontAwesome'> </span><span font='9' color='#131e20' bgcolor='#4280c8'>$(date '+%m/%d/%y %H:%M') </span>"
