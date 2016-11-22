fbf43ae29dcacc09262d451d2a06965fef2d0919
Weznon <waterwasp55555@gmail.com>
2016-11-22 18:45:14 -0500

    fixed functions returning null

--- .config/i3/scripts/netid.sh
+++ .config/i3/scripts/netid.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-echo "<span font='9' color='#131e20' background='#2e63a0'>  $(iwgetid -r) </span>"
+if [[ $(iwgetid -r) ]]; then
+  echo "<span font='9' color='#131e20' background='#2e63a0'>  $(iwgetid -r) </span>"
+else
+  echo "<span font='9' color='#131e20' background='#2e63a0'>   N/A </span>"
+fi 
--- .config/i3/scripts/volume.sh
+++ .config/i3/scripts/volume.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "<span color='#131e20' bgcolor='#6da090' font_desc='FontAwesome'> </span><span color='#131e20' bgcolor='#6da090' font='9'>$(amixer get Master | grep -m 1 -oE '\[[0-9][0-9]\%\]'| head -1)</span>"
+echo "<span color='#131e20' bgcolor='#6da090' font_desc='FontAwesome'> </span><span color='#131e20' bgcolor='#6da090' font='9'>$(amixer get Master | grep -m 1 -oE '\[[0-9]*\%\]'| head -1)</span>"
